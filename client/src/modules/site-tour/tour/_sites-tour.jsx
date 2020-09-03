243c268f7587dbdb5cccbe5d9dc85945e19d4506
Zach Smith <zachsmith.sa@gmail.com>
2020-09-03 11:44:08 +0200

    Fixed minor tour problems

--- client/src/modules/site-tour/tour/_sites-tour.jsx
+++ client/src/modules/site-tour/tour/_sites-tour.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default [
   {
+    dialogProps: {
+      width: 600,
+    },
     id: 'site-tour',
     pathname: '/sites',
     title: 'Search by Sites',
@@ -12,6 +15,7 @@ export default [
         map (as apposed to clicking a cluster, which shows data for selected sites). You can remove
         search parameters by clicking the reset button (enabled when search parameters have been
         applied). The button in the bottom right shows results that match the current search context
+        (enabled when there is 1+ results)
       </p>
     ),
     goto: 'the-end',
